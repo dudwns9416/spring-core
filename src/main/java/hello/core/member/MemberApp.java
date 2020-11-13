694abd6ca56434049d4fdc4aa9b6a9f61433c369
dudwns9416 <dudwns9416@naver.com>
2020-11-13 21:13:41 +0900

    [core-1]스프링 원리 이해하기

---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,13 @@
+package hello.core.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl(null);
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        final Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
